e9824453ee4917c761f7d6ad08dbcf9eaad5edc2
ajithshine <ajithshine77@gmail.com.com>
2021-12-31 00:44:29 +0530

    Events

---
+++ FrontEnd/front/src/Event.js
@@ -0,0 +1,60 @@
+import './images';
+import './Event.css'
+
+function App() {
+    return (
+        <div>
+            <div class="event">
+    <p>Events</p>
+  </div>
+  <div class="section2">
+  <div class = container>
+    <div class = card>
+      <div class = image>
+        <img href = "#" src = './general.png' />
+      </div>
+      <div class = content>
+        <a class="view" href="#">
+          <span>view</span>
+          <span>view</span>
+        </a>
+      </div>
+    </div>
+    <div class = card>
+      <div class = image>
+        <img href = "#" src = './ece.png' />
+      </div>
+      <div class = content>
+        <a class="view" href="#">
+          <span>view</span>
+          <span>view</span>
+        </a>
+      </div>
+    </div>
+    <div class = card>
+      <div class = image>
+        <img href = "#" src = './eee.png' />
+      </div>
+      <div class = content>
+        <a class="view" href="#">
+          <span>view</span>
+          <span>view</span>
+        </a>
+      </div>
+    </div>  
+    <div class = card>
+      <div class = image>
+        <img href = "#" src =   './computer.png' />
+      </div>
+      <div class = content>
+        <a class="view" href="#">
+          <span>view</span>
+          <span>view</span>
+        </a>
+      </div>
+    </div>   
+  </div>
+</div>
+        </div>    
+    );
+}
