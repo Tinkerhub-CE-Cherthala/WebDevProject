561750758d2ef97ac7d38aac70bc394a53ae5f25
ajithshine <ajithshine77@gmail.com.com>
2021-12-31 01:37:25 +0530

    correction in event

--- FrontEnd/front/src/Event.js
+++ FrontEnd/front/src/Event.js
@@ -8,45 +8,45 @@ function Event() {
     <p>Events</p>
   </div>
   <div class="section2">
-  <div class = container>
-    <div class = card>
-      <div class = image>
+  <div class = "container">
+    <div class = "card">
+      <div class = "image">
         <img href = "#" src = './general.png' />
       </div>
-      <div class = content>
+      <div class = "content">
         <a class="view" href="#">
           <span>view</span>
           <span>view</span>
         </a>
       </div>
     </div>
-    <div class = card>
-      <div class = image>
+    <div class = "card">
+      <div class = "image">
         <img href = "#" src = './ece.png' />
       </div>
-      <div class = content>
+      <div class = "content">
         <a class="view" href="#">
           <span>view</span>
           <span>view</span>
         </a>
       </div>
     </div>
-    <div class = card>
-      <div class = image>
+    <div class = "card">
+      <div class = "image">
         <img href = "#" src = './eee.png' />
       </div>
-      <div class = content>
+      <div class = "content">
         <a class="view" href="#">
           <span>view</span>
           <span>view</span>
         </a>
       </div>
     </div>  
-    <div class = card>
-      <div class = image>
+    <div class = "card">
+      <div class = "image">
         <img href = "#" src =   './computer.png' />
       </div>
-      <div class = content>
+      <div class = "content">
         <a class="view" href="#">
           <span>view</span>
           <span>view</span>
